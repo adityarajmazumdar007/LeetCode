8f0d7698bc720bd65e0485bc287eeb1cc3cb2555
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-01-26 01:01:17 -0800

    Time: 7 ms (38.59%) | Memory: 44.7 MB (54.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -6,13 +6,18 @@ class Solution {
         int end =0;
         int ans= -1;
         while(end<s.length()){
-            mp.put(s.charAt(end),mp.getOrDefault(s.charAt(end),0)+1);
-            while(mp.size()<end-start+1){
-                mp.put(s.charAt(start),mp.get(s.charAt(start))-1);
-                if(mp.get(s.charAt(start))==0){mp.remove(s.charAt(start));}
+            int windowSize = end-start+1;
+            char ch = s.charAt(end);
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
+            while(mp.size()< windowSize) {
+                char ch1 = s.charAt(start);
+                mp.put(ch1,mp.get(ch1)-1);
+                if(mp.get(ch1)==0){mp.remove(ch1);}
                 start++;
+                windowSize = end-start+1;
             }
-            ans=Math.max(ans,end-start+1);    
+            windowSize = end-start+1;
+            ans=Math.max(ans,windowSize);    
             end++;
         }
         return ans;
