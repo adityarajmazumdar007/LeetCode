711df7c3aac8e1aada900ed9d54c2e0a80fe5047
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-02-01 19:25:43 -0800

    Time: 4 ms (98.16%) | Memory: 60.1 MB (13.50%) - LeetSync

---
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        long res = 0;
+        long prefixSum = 0;
+        for(int i =0; i< nums.length; i++ ) {
+            prefixSum += nums[i];
+            res= Math.max(res, (prefixSum+i)/(i+1));
+        }
+        return (int)res;
+    }
+}
