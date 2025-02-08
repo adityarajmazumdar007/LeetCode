b4d3225a3c0e9f1af7ba876e317f8f9c7e87c45f
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-02-08 08:29:03 -0800

    Time: 0 ms (100.00%) | Memory: 42.5 MB (26.53%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -11,9 +11,6 @@ class Solution {
                 nums1[k--] = nums2[j--];
             }
         }
-        while (i >= 0) {
-            nums1[k--] = nums1[i--];
-        }
         while( j >=0 ) {
             nums1[k--] = nums2[j--];
         }
