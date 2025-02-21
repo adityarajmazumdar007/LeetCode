90be3c765c8d22cbab66db1ea580c37db3c3b087
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-02-21 03:55:47 -0800

    Time: 24 ms (36.12%) | Memory: 47.9 MB (27.76%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
@@ -11,8 +11,8 @@ class FindElements {
     public void dfs(TreeNode root, int val){
         if(root==null) return;
         root.val = val;
-        set.add(val);
         dfs(root.left, 2*val+1);
         dfs(root.right, 2*val+2);
+        set.add(val);
     }
 }
