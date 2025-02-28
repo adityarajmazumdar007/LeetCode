79f66083b2286ef0a4066ad98ae93404b5b1a941
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-02-28 07:47:43 -0800

    Time: 44 ms (53.45%) | Memory: 55.6 MB (19.82%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+     private static int compare(String text1, String text2,int index1,int index2,int storageSystem[][]){
+       if(index1 < 0 || index2< 0 )return 0;
+       if(storageSystem[index1][index2]!=-1)return storageSystem[index1][index2];
+       if(text1.charAt(index1)==text2.charAt(index2))return storageSystem[index1][index2]=1+compare(text1,text2,index1-1,index2-1,storageSystem);
+       return storageSystem[index1][index2]= Math.max(compare(text1,text2,index1-1,index2,storageSystem),compare(text1,text2,index1,index2-1,storageSystem));
+       
+   }
+    public int longestPalindromeSubseq(String text1) {
+        StringBuilder sb = new StringBuilder(text1);
+        String text2 = sb.reverse().toString();
+        int n= text1.length();
+        int m = text2.length();
+        int storageSystem[][]= new int[n][m];
+        for(int arr[]:storageSystem){
+            Arrays.fill(arr,-1);
+        }
+        return compare(text1,text2,n-1,m-1,storageSystem);
+    }
+}
