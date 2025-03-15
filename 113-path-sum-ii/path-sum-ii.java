49864300fdb7fea5f52e20b374464d97e88d9e52
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-03-15 05:33:45 -0700

    Time: 1 ms (99.85%) | Memory: 44.6 MB (72.20%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -1,13 +1,11 @@
 class Solution {
     public static void  check(TreeNode root, int sum, int target,List<List<Integer>>  ans, List<Integer> temp){
         if(root==null)return ;
-        if(root.left == null && root.right==null){
-            target=target+root.val;
-            if(sum==target){
+        if(root.left == null && root.right==null
+            && sum==target + root.val){
                 temp.add(root.val);
                 ans.add(new ArrayList<>(temp));
                 temp.remove(temp.size()-1);
-            }
         }
         temp.add(root.val);
         check(root.left,sum,target+root.val,ans,temp);
