9e49c1c7120af05b4b341da0d46d396e60d19256
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-03-21 18:38:36 -0700

    Time: 0 ms (100.00%) | Memory: 40.4 MB (60.34%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,20 +1,18 @@
 class Solution {
-     private static int solve(int row, int coloumn,int storageSystem [][]){
-        if( row==0 && coloumn ==0)return 1;
-        if(row< 0 || coloumn < 0)return 0;
-        if(storageSystem[row][coloumn]!=-1)return storageSystem[row][coloumn];
-        
-        int upwards = solve(row-1,coloumn,storageSystem);
-        int leftwards = solve(row,coloumn-1,storageSystem);
-        return storageSystem[row][coloumn]=upwards+leftwards;
-        
-    }
-    
     public int uniquePaths(int m, int n) {
-        int storageSystem [][] = new int[m][n];
-        for (int i = 0; i < m; i++) {
-        Arrays.fill(storageSystem[i], -1);
+        int dp[][] = new int[n][m];
+        dp[0][0] = 1;
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = dp[i-1][0] ; 
         }
-        return solve(m-1,n-1,storageSystem);
+        for(int j = 1; j < m; j++) {
+            dp[0][j] = dp[0][j-1];
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
