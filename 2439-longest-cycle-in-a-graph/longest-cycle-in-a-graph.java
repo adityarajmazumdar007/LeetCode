64b793810b18f60a2416b3241889c23a8dd3fe09
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-03-24 15:55:26 -0700

    Time: 25 ms (64.78%) | Memory: 67.8 MB (54.43%) - LeetSync

---
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    private int result = -1;
+
+    private void dfs(int u, int[] edges, boolean[] visited, int[] dist, boolean[] inRecursion) {
+        if (u != -1) {
+            visited[u] = true;
+            inRecursion[u] = true;
+
+            int v = edges[u];
+
+            if (v != -1 && !visited[v]) {
+                dist[v] = dist[u] + 1;
+                dfs(v, edges, visited, dist, inRecursion);
+            } else if (v != -1 && inRecursion[v]) { // Cycle detected
+                result = Math.max(result, dist[u] - dist[v] + 1);
+            }
+
+            inRecursion[u] = false;
+        }
+    }
+
+    public int longestCycle(int[] edges) {
+        int n = edges.length;
+        boolean[] visited = new boolean[n];
+        int[] dist = new int[n];
+        Arrays.fill(dist, 1);
+        boolean[] inRecursion = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, edges, visited, dist, inRecursion);
+            }
+        }
+
+        return result;
+    }
+}
