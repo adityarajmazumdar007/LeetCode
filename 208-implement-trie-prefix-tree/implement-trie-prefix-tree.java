0f17f1d7446a043011a32c6c9f3e01d5885eb7d8
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-04-04 20:31:32 +0530

    Time: 34 ms (76.29%) | Memory: 55.4 MB (70.29%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -4,65 +4,54 @@ class Trie {
         boolean isEndOfWord;
 
         TrieNode() {
-            children = new TrieNode[26];
             isEndOfWord = false;
+           children = new TrieNode[26];
         }
     }
 
     private TrieNode root;
 
-    Trie() {
+    /** Initialize your data structure here. */
+    public Trie() {
         root = new TrieNode();
     }
 
-    private TrieNode getNode() {
-        return new TrieNode();
-    }
-
+    /** Inserts a word into the trie. */
     public void insert(String word) {
         TrieNode crawler = root;
-
-        for (int i = 0; i < word.length(); i++) {
-            int index = word.charAt(i) - 'a';
-            if (crawler.children[index] == null)
-                crawler.children[index] = getNode();
-
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (crawler.children[index] == null) {
+                crawler.children[index] = new TrieNode();
+            }
             crawler = crawler.children[index];
         }
-
         crawler.isEndOfWord = true;
     }
 
+    /** Returns if the word is in the trie. */
     public boolean search(String word) {
         TrieNode crawler = root;
-
-        for (int i = 0; i < word.length(); i++) {
-            int index = word.charAt(i) - 'a';
-            if (crawler.children[index] == null)
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (crawler.children[index] == null) {
                 return false;
+            }
             crawler = crawler.children[index];
         }
-        return crawler != null && crawler.isEndOfWord;
+        return crawler.isEndOfWord;
     }
 
+    /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
         TrieNode crawler = root;
-        int i = 0;
-        for (i = 0; i < prefix.length(); i++) {
-            int index = prefix.charAt(i) - 'a';
-
-            if (crawler.children[index] == null)
+        for (char ch : prefix.toCharArray()) {
+            int index = ch - 'a';
+            if (crawler.children[index] == null) {
                 return false;
-
+            }
             crawler = crawler.children[index];
         }
-        return i == prefix.length();
+        return true;
     }
 }
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
