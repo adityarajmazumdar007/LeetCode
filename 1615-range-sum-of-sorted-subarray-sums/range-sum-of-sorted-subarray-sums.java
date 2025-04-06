970bac54881a3a44831c8c1f551efb65ceeb41fa
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-04-06 18:15:50 +0530

    Time: 81 ms (48.24%) | Memory: 45.2 MB (78.52%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,29 @@
+class Solution {
+    private static final int M = (int)1e9 + 7;
+
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+
+        // Initialize the priority queue with the initial subarray sums
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[]{nums[i], i});
+        }
+
+        int result = 0;
+
+        for (int i = 1; i <= right; i++) {
+            int[] current = pq.poll();
+
+            // If the current index is within the desired range, add the value to the result
+            if (i >= left) {
+                result = (result + current[0]) % M;
+            }
+
+            // If index is less than the last index, increment it and add the new subarray sum to the queue
+            if (current[1] < n - 1) {
+                pq.add(new int[]{current[0] + nums[current[1] + 1], current[1] + 1});
+            }
+        }
+        return result;
+    }
+}
