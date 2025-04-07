87c5e87bffcb62940ed1e0f7c34f78523e9caa31
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-04-07 17:10:06 +0530

    Time: 90 ms (73.02%), Space: 138.4 MB (7.67%) - LeetHub

--- 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.java
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.java
@@ -1,22 +1,46 @@
-import java.util.*;
-
 class Solution {
-    public boolean checkValidCuts(int n, int[][] rectangles) {
-        return canCut(rectangles, 0) || canCut(rectangles, 1);
-    }
+    public int[][] merge(int[][] intervals) {
+        int n = intervals.length;
 
-    private boolean canCut(int[][] rectangles, int axis) {
-        Arrays.sort(rectangles, Comparator.comparingInt(a -> a[axis]));
-        int cuts = 0, previousEnd = -1;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        for (int[] rect : rectangles) {
-            int start = rect[axis], end = rect[axis + 2];
+        List<int[]> result = new ArrayList<>();
+        result.add(intervals[0]);
 
-            if (start >= previousEnd) cuts++;
-            previousEnd = Math.max(previousEnd, end);
-            if (cuts >= 3) return true;
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] < result.get(result.size() - 1)[1]) { // Overlapping
+                result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], intervals[i][1]);
+            } else {
+                result.add(intervals[i]);
+            }
         }
 
-        return false;
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public boolean checkValidCuts(int n, int[][] rectangles) {
+        List<int[]> hor = new ArrayList<>();
+        List<int[]> vert = new ArrayList<>();
+
+        for (int[] coord : rectangles) {
+            int x1 = coord[0];
+            int y1 = coord[1];
+            int x2 = coord[2];
+            int y2 = coord[3];
+
+            hor.add(new int[]{x1, x2});
+            vert.add(new int[]{y1, y2});
+        }
+
+        int[][] horArray = hor.toArray(new int[hor.size()][]);
+        int[][] vertArray = vert.toArray(new int[vert.size()][]);
+
+        int[][] result1 = merge(horArray);
+        if (result1.length >= 3) {
+            return true;
+        }
+
+        int[][] result2 = merge(vertArray);
+        return result2.length >= 3;
     }
 }
