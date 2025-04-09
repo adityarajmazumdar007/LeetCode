0e0481a04fc1015cbf2fa626cb4257b28ce8c714
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-04-09 17:19:54 +0530

    Time: 197 ms (22.97%), Space: 48.4 MB (22.85%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,38 +1,43 @@
 class Solution {
-    public List < String > findAdj (String node, HashSet < String > visited) {
-         List < String > adj = new ArrayList <> ();
+    public List<String> findAdj(String node, Set<String> wordSet) {
+        List<String> adj = new ArrayList<>();
         for (int i = 0; i < node.length(); i++) {
-            for ( char ch = 'a' ; ch <='z' ; ch++) {
-                if ( ch == node.charAt(i)) continue;
-                String newWord = node.substring(0,i) + ch + node.substring(i+1,node.length());
-                if (visited.contains(newWord) ) adj.add(newWord);
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                if (ch == node.charAt(i)) continue;
+                String newWord = node.substring(0, i) + ch + node.substring(i + 1);
+                if (wordSet.contains(newWord)) adj.add(newWord);
             }
         }
         return adj;
     }
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet < String > visited = new HashSet < String > (wordList);
-        if ( !visited.contains(endWord) ) return 0;
-        Queue < String > q = new LinkedList<>();
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return 0;
+
+        Queue<String> q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
         q.offer(beginWord);
-        if ( visited.contains(beginWord) ) visited.remove(beginWord);
-        int level = 0;
-        while ( q.size() !=0 ) {
-            int currLevelSize = q.size();
-            for ( int i = 0; i < currLevelSize; i++ ) {
-                String  node = q.poll ();
-                if (node.equals(endWord)) { 
-                return level + 1; }
-                List < String > neighbours = findAdj ( node, visited) ;
-                for ( String neighbour : neighbours ) {
-                    if ( visited.contains(neighbour)) {
-                        q.offer ( neighbour );
-                        visited.remove( neighbour );
+        visited.add(beginWord);
+        int level = 1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String node = q.poll();
+                if (node.equals(endWord)) return level;
+
+                for (String neighbor : findAdj(node, wordSet)) {
+                    if (!visited.contains(neighbor)) {
+                        visited.add(neighbor);
+                        q.offer(neighbor);
                     }
                 }
             }
             level++;
         }
+
         return 0;
     }
 }
