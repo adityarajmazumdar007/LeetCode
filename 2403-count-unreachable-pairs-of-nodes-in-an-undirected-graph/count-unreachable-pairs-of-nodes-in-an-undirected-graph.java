b135a875e424f69622ff4687f46c0f1a702660cd
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-04-10 18:01:55 +0530

    Time: 26 ms (74.91%) | Memory: 95.1 MB (94.18%) - LeetSync

---
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Solution {
+    class DSU {
+        int[] parent;
+        int[] size;
+
+        DSU(int n) {
+            parent = new int[n];
+            size = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int findUltimateParent(int node) {
+            if (node == parent[node]) {
+                return node;
+            }
+            // Path Compression
+            return parent[node] = findUltimateParent(parent[node]);
+        }
+
+        public boolean unionBySize(int node1, int node2) {
+            int ultimateParent1 = findUltimateParent(node1);
+            int ultimateParent2 = findUltimateParent(node2);
+
+            if (ultimateParent1 == ultimateParent2) return false;
+
+            if (size[ultimateParent1] < size[ultimateParent2]) {
+                parent[ultimateParent1] = ultimateParent2;
+                size[ultimateParent2] += size[ultimateParent1];
+            } else {
+                parent[ultimateParent2] = ultimateParent1;
+                size[ultimateParent1] += size[ultimateParent2];
+            }
+
+            return true;
+        }
+    }
+
+    public long countPairs(int n, int[][] edges) {
+        DSU disjointSet = new DSU(n);
+
+        for (int[] edge : edges) {
+            disjointSet.unionBySize(edge[0], edge[1]);
+        }
+
+        // Count size of each connected component
+        HashMap<Integer, Integer> componentSize = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int parent = disjointSet.findUltimateParent(i);
+            componentSize.put(parent, componentSize.getOrDefault(parent, 0) + 1);
+        }
+
+        // Count unreachable pairs
+        List<Integer> sizes = new ArrayList<>(componentSize.values());
+        long result = 0;
+        long suffixSum = n;
+
+        for (int size : sizes) {
+            suffixSum -= size;
+            result += (long) size * suffixSum;
+        }
+
+        return result;
+    }
+}
