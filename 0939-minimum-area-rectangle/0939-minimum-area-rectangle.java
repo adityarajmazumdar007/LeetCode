45cdb07299b6b2370f20c60a2a9543c3f9c68db4
adityarajmazumdar007 <108982093+adityarajmazumdar007@users.noreply.github.com>
2025-05-03 23:54:27 +0530

    Time: 413 ms (40.52%), Space: 44.9 MB (86.23%) - LeetHub

---
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+    public class Node{
+        int x;
+        int y;
+        Node(int a,int b) {x=a;y=b;}
+        
+        @Override
+        public boolean equals(Object obj)
+        { //System.out.println("inside");
+            return (((Node) obj).x == this.x)&&(((Node) obj).y == this.y);
+        }
+        
+        @Override
+        public int hashCode(){
+            return Objects.hash(x,y);
+        }
+
+    }
+    
+    public int minAreaRect(int[][] points) {
+        int min=Integer.MAX_VALUE;
+        Set<Node> ptset = new HashSet<>();
+        for(int i=0;i<points.length;i++)
+            ptset.add(new Node(points[i][0],points[i][1]));
+        for(int i=0;i<points.length;i++)
+            for(int j=i+1;j<points.length;j++)
+            {
+                int x1= points[i][0];
+                int y1= points[i][1];
+                int x2= points[j][0];
+                int y2= points[j][1];
+                Node pt3 = new Node(x1,y2);
+                Node pt4 = new Node(x2,y1);
+                if(ptset.contains(pt3) && ptset.contains(pt4))
+                {   int area= Math.abs(x1-x2)*Math.abs(y1-y2);
+                    //System.out.println("Inside "+ x1 +" "+y1+" "+x2+" "+y2+" "+area);
+                    
+                    if(min>area && area!=0)
+                        min=area;
+                }
+            }
+        return (min==Integer.MAX_VALUE ? 0:min);
+    }
+}
